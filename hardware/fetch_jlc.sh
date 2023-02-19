deb640123738fa75c059069aa4ef2596cc934c00
Thomas Buck <xythobuz@xythobuz.de>
2023-02-19 20:03:22 +0100

    continue work on pi part

--- hardware/fetch_jlc.sh
+++ hardware/fetch_jlc.sh
@@ -19,6 +19,19 @@ JLC2KiCadLib \
     C172043 \
     C105883 \
     C83063 \
+    C2920042 \
+    C520861 \
+    C406733 \
+    C380316 \
+    C144787 \
+    C2937625 \
+    C234182 \
+    C146690 \
+    C40877 \
+    C144481 \
+    C697422 \
+    C237221 \
+    C106200 \
     -dir jlc_lib \
     -schematic_lib jlc_schematics \
     -footprint_lib jlc_footprints \
