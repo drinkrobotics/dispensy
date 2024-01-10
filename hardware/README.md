68b453902ede44f3fab83be3137547a2ba842b5e
Thomas Buck <xythobuz@xythobuz.de>
2024-01-10 19:00:15 +0100

    remove jlc_lib from repo

---
+++ hardware/README.md
@@ -0,0 +1,8 @@
+# Hardware
+
+This is a KiCAD 7 hierarchical schematic and layout.
+Take a look at the [docs](https://docs.kicad.org/7.0/en/eeschema/eeschema.html#hierarchical-schematics).
+
+The repeated schematic sheets can also be replicated in the layout using [this plugin](https://github.com/MitjaNemec/ReplicateLayout).
+
+To fetch symbols, footprints and 3d models automatically from [the JLCPCB parts library](https://jlcpcb.com/parts), [JLC2KiCad_lib](https://github.com/TousstNicolas/JLC2KiCad_lib) is used with the `fetch_jlc.sh` script.
--- hardware/fetch_jlc.sh
+++ hardware/fetch_jlc.sh
@@ -2,44 +2,51 @@
 
 # https://github.com/TousstNicolas/JLC2KiCad_lib
 
+# enter directory of script
+cd "$(dirname "$0")"
+
+# create output directory
+mkdir -p jlc_lib
+
+# fetch / update parts libs
 JLC2KiCadLib \
-    C2040 \
-    C113767 \
-    C26537 \
-    C22452 \
-    C132227 \
-    C86580 \
-    C194472 \
-    C134092 \
-    C386740 \
-    C148313 \
-    C279981 \
-    C140675 \
-    C409700 \
-    C172043 \
-    C105883 \
-    C83063 \
-    C2920042 \
-    C520861 \
-    C406733 \
-    C380316 \
-    C144787 \
-    C2937625 \
-    C234182 \
-    C146690 \
-    C40877 \
-    C144481 \
-    C697422 \
-    C237221 \
-    C106200 \
-    C5613 \
-    C2909114 \
-    C475721 \
-    C497903 \
-    C249838 \
-    C310843 \
+    C2040    `# RP2040` \
+    C113767  `# W25Q128JVSIQ 128Mbit SPI NOR FLASH` \
+    C26537   `# NCP1117ST33T3G 3.3V 1A LDO !! LOW STOCK !!` \
+    C22452   `# SS54 40V 5A Schottky Barrier Diode` \
+    C132227  `# ` \
+    C86580   `# ` \
+    C194472  `# ` \
+    C134092  `# ` \
+    C386740  `# ` \
+    C148313  `# ` \
+    C279981  `# ` \
+    C140675  `# ` \
+    C409700  `# ` \
+    C172043  `# ` \
+    C105883  `# ` \
+    C83063   `# ` \
+    C2920042 `# ` \
+    C520861  `# ` \
+    C406733  `# ` \
+    C380316  `# ` \
+    C144787  `# ` \
+    C2937625 `# ` \
+    C234182  `# ` \
+    C146690  `# ` \
+    C40877   `# ` \
+    C144481  `# ` \
+    C697422  `# ` \
+    C237221  `# ` \
+    C106200  `# ` \
+    C5613    `# ` \
+    C2909114 `# ` \
+    C475721  `# ` \
+    C497903  `# ` \
+    C249838  `# ` \
+    C310843  `# ` \
     -dir jlc_lib \
-    -schematic_lib jlc_schematics \
+    -symbol_lib jlc_schematics \
+    -symbol_lib_dir Schematic \
     -footprint_lib jlc_footprints \
     --skip_existing
-
